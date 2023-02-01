8e0844de5c2143a06150216a7eb436202f2f1be4
armazenamentoproig <119739333+armazenamentoproig@users.noreply.github.com>
2023-02-01 20:49:15 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,16 @@
 <?php
-  $mysqli = mysqli_init();
-  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-  $mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["DATABASE"]);
-  $mysqli->close();
-?>
+$hostname = $_ENV['aws-sa-east-1.connect.psdb.cloud'];
+$dbName = $_ENV['formulario'];
+$username = $_ENV['ioudoj7c787zzow91xbp'];
+$password = $_ENV['pscale_pw_b9NXLvxy1gtj3EJA7JrPitgv48QPwqeXjm03FzDKjH8'];
+$ssl = $_ENV['MYSQL_ATTR_SSL_CA'];
+
+$mysqli = mysqli_init();
+$mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
+$mysqli->real_connect($hostname, $username, $password, $dbName, $port);
+
+if ($mysqli->connect_error) {
+    echo 'not connected to the database';
+} else {
+    echo "Connected successfully";
+}
