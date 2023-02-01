6e625b7cd78c6e604e7269e2df05b795946dd723
armazenamentoproig <119739333+armazenamentoproig@users.noreply.github.com>
2023-02-01 18:12:30 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "celke";
+$servidor = "aws-sa-east-1.connect.psdb.cloud";
+$usuario = "smwtjehkm3ky0z8zzbvj";
+$senha = "pscale_pw_A6tAIL5oSE6usBu54IdCcfLyDMxDhYMiE0t4Wn8THnc";
+$dbname = "formulario";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
