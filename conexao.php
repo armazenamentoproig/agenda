10845f02c207059d95c70c450139196aee7ad8d9
armazenamentoproig <119739333+armazenamentoproig@users.noreply.github.com>
2023-02-01 18:21:37 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,12 @@
 <?php
-$servidor = "aws-sa-east-1.connect.psdb.cloud";
-$usuario = "smwtjehkm3ky0z8zzbvj";
-$senha = "pscale_pw_A6tAIL5oSE6usBu54IdCcfLyDMxDhYMiE0t4Wn8THnc";
-$dbname = "formulario";
+$HOST = "aws-sa-east-1.connect.psdb.cloud";
+$USERNAME = "smwtjehkm3ky0z8zzbvj";
+$PASSWORD = "pscale_pw_A6tAIL5oSE6usBu54IdCcfLyDMxDhYMiE0t4Wn8THnc";
+$DATABASE = "formulario";
 
 //Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+ $mysqli = mysqli_init();
+  $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
+  $mysqli->real_connect($["HOST"], $["USERNAME"], $["PASSWORD"], $["DATABASE"]);
+  $mysqli->close();
+?>
