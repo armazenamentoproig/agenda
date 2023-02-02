95a836a1e86def269767937d65c2fed1a2fb3e74
armazenamentoproig <119739333+armazenamentoproig@users.noreply.github.com>
2023-02-01 21:11:27 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@ $hostname=$_ENV['HOST'];
 $dbName = $_ENV['DATABASE'];
 $username = $_ENV['USERNAME'];
 $password = $_ENV['PASSWORD'];
-$port = $_ENV[''];
+$port = $_ENV['3306'];
 $ssl = $_ENV['MYSQL_ATTR_SSL_CA'];
 
 // Set SSL cert and open connection to the MySQL server
