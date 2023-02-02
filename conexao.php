3ef69d3fdf68682956f4699069d4e94d79341bf3
armazenamentoproig <119739333+armazenamentoproig@users.noreply.github.com>
2023-02-01 22:48:01 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
-$hostname=$.ENV['HOST'];
-$dbName = $.ENV['DATABASE'];
-$username = $.ENV['USERNAME'];
-$password = $.ENV['PASSWORD'];
-$port = $.ENV['3306'];
-$ssl = $.ENV['MYSQL_ATTR_SSL_CA'];
+$hostname=$_ENV['HOST'];
+$dbName = $_ENV['DATABASE'];
+$username = $_ENV['USERNAME'];
+$password = $_ENV['PASSWORD'];
+$port = $_ENV['3306'];
+$ssl = $_ENV['MYSQL_ATTR_SSL_CA'];
 
 // Set SSL cert and open connection to the MySQL server
 $mysqli = mysqli_init();
 $mysqli->ssl_set(NULL, NULL, $ssl, NULL, NULL);
-$mysqli->real_connect($hostname, $username, $password, $dbName, $port);
+$mysqli->real_connect($hostname, $dbName, $username, $password, $port);
 
 if ($mysqli->connect_error) {
     echo "Not connected to the database";
