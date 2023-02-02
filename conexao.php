873742c85b36f10c41aaffd6b2946b17cd0dfff3
armazenamentoproig <119739333+armazenamentoproig@users.noreply.github.com>
2023-02-01 23:05:03 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,13 @@
 <?php
 error_reporting(E_STRICT);
+HOSTNAME = "aws-sa-east-1.connect.psdb.cloud"
+DATABASE = "formulario"
+USERNAME = "ioudoj7c787zzow91xbp"
+PASSWORD = "pscale_pw_b9NXLvxy1gtj3EJA7JrPitgv48QPwqeXjm03FzDKjH8"
 
 $mysqli = mysqli_init();
 $mysqli->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
-$mysqli->real_connect('[aws-sa-east-1.connect.psdb.cloud]', '[ioudoj7c787zzow91xbp]', '[pscale_pw_b9NXLvxy1gtj3EJA7JrPitgv48QPwqeXjm03FzDKjH8]', '[formulario]');
+$mysqli->real_connect('[HOSTNAME]', '[USERNAME]', '[PASSWORD]', '[DATABASE]');
 $mysqli->close();
 ?>
 Successfully connected to PlanetScale!
